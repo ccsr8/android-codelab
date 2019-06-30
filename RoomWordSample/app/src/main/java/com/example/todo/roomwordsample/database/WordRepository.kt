723c0e358403f874ff3b2ca74c1b0db1548334ab
chatchai <chatchai4566@gmail.com>
2019-06-30 22:29:09 +0700

    Created - Add ViewModel

--- RoomWordSample/app/src/main/java/com/example/todo/roomwordsample/database/WordRepository.kt
+++ RoomWordSample/app/src/main/java/com/example/todo/roomwordsample/database/WordRepository.kt
@@ -7,7 +7,7 @@ class WordRepository(private val wordDao: WordDao) {
     val allWords: LiveData<List<Word>> = wordDao.getAllWords()
 
     @WorkerThread
-    suspend fun insert(word: Word){
+    suspend fun insert(word: Word) {
         wordDao.insert(word)
     }
 }
